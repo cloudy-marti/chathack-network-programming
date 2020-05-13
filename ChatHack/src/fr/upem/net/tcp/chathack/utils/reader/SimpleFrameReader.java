8ea94cf4738bd0ff80b6822afdcb8a7e81f8189e
Cloudy <cloud.kira@gmail.com>
2020-05-13 18:22:28 +0200

    added basic readers

---
+++ ChatHack/src/fr/upem/net/tcp/chathack/utils/reader/SimpleFrameReader.java
@@ -0,0 +1,66 @@
+package fr.upem.net.tcp.chathack.utils.reader;
+
+import fr.upem.net.tcp.chathack.utils.frame.MessageFrame;
+import fr.upem.net.tcp.chathack.utils.frame.SimpleFrame;
+
+import java.nio.ByteBuffer;
+
+/*
+                  byte     int   String
+                -----------------------
+                | Opcode | Size | Msg |
+                -----------------------
+
+                ErrorFrame/AckFrame/ConnectionFrame/ResponseFrame
+ */
+
+public class SimpleFrameReader implements Reader<SimpleFrame> {
+
+    private enum State {DONE, WAITING, ERROR};
+
+    private State state = State.WAITING;
+    private static final int BUFFER_SIZE = 1_024;
+    private final ByteBuffer internalBuffer = ByteBuffer.allocate(BUFFER_SIZE); // write-mode
+
+    private String message;
+    private int opcode;
+
+    @Override
+    public ProcessStatus process(ByteBuffer buffer) {
+        if (state == State.DONE || state == State.ERROR) {
+            throw new IllegalStateException();
+        }
+
+        opcode = buffer.get() & 0xFF;
+
+        StringReader stringReader = new StringReader();
+        if(state == State.WAITING) {
+            ProcessStatus status = stringReader.process(buffer);
+            switch (status) {
+                case DONE:
+                    message = stringReader.get();
+                    state = State.DONE;
+                    return ProcessStatus.DONE;
+                case REFILL:
+                    return ProcessStatus.REFILL;
+                case ERROR:
+                    return ProcessStatus.ERROR;
+            }
+        }
+        return ProcessStatus.DONE;
+    }
+
+    @Override
+    public SimpleFrame get() {
+        if (state != State.DONE) {
+            throw new IllegalStateException();
+        }
+        return new SimpleFrame(opcode, message);
+    }
+
+    @Override
+    public void reset() {
+        state = State.WAITING;
+        internalBuffer.clear();
+    }
+}
---
+++ ChatHack/src/fr/upem/net/tcp/chathack/utils/reader/StringReader.java
@@ -0,0 +1,88 @@
+package fr.upem.net.tcp.chathack.utils.reader;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class StringReader implements Reader<String> {
+
+    private enum State {DONE, WAITING_SIZE, WAITING_BYTES, ERROR};
+
+    private State state = State.WAITING_SIZE;
+    private static final int BUFFER_SIZE = 1_024;
+    private final ByteBuffer internalBuffer = ByteBuffer.allocate(BUFFER_SIZE); // write-mode
+    private int size;
+    private String value;
+
+    /* Format :
+     * +------------+------------+
+     * | Size (INT) |  Text UTF8 |
+     * +------------+------------+
+     */
+    @Override
+    public ProcessStatus process(ByteBuffer buffer) {
+        if (state == State.DONE || state == State.ERROR) {
+            throw new IllegalStateException();
+        }
+
+        if(state == State.WAITING_SIZE) {
+            //internalBuffer.limit(Integer.BYTES);
+            IntReader sizeReader = new IntReader();
+            ProcessStatus status = sizeReader.process(buffer);
+            switch (status) {
+                case DONE:
+                    size = sizeReader.get();
+                    if(size <= BUFFER_SIZE && size >= 0) {
+                        state = State.WAITING_BYTES;
+                        break;
+                    } else {
+                        state = State.ERROR;
+                        return ProcessStatus.ERROR;
+                    }
+                case REFILL:
+                    return ProcessStatus.REFILL;
+                case ERROR:
+                    //return ProcessStatus.ERROR;
+                    throw new AssertionError();
+            }
+        }
+
+        buffer.flip();
+        internalBuffer.limit(size);
+        try {
+            if(buffer.remaining() <= internalBuffer.remaining()) {
+                internalBuffer.put(buffer);
+            } else {
+                int oldLimit = buffer.limit();
+                buffer.limit(internalBuffer.remaining());
+                internalBuffer.put(buffer);
+                buffer.limit(oldLimit);
+            }
+        } finally {
+            buffer.compact();
+        }
+
+        if(internalBuffer.position() < size) {
+            return ProcessStatus.REFILL;
+        }
+        state = State.DONE;
+        internalBuffer.flip();
+        internalBuffer.limit(size);
+        value = StandardCharsets.UTF_8.decode(internalBuffer).toString();
+
+        return ProcessStatus.DONE;
+    }
+
+    @Override
+    public String get() {
+        if (state != State.DONE) {
+            throw new IllegalStateException();
+        }
+        return value;
+    }
+
+    @Override
+    public void reset() {
+        state = State.WAITING_SIZE;
+        internalBuffer.clear();
+    }
+}
