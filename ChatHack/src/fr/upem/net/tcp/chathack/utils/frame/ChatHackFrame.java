1a0748c362a0f04c14214523e253ea98c5a51b3b
Cloudy <cloud.kira@gmail.com>
2020-05-19 11:58:11 +0200

    added constant opcodes

--- ChatHack/src/fr/upem/net/tcp/chathack/utils/frame/ChatHackFrame.java
+++ ChatHack/src/fr/upem/net/tcp/chathack/utils/frame/ChatHackFrame.java
@@ -9,4 +9,25 @@ public interface ChatHackFrame {
     boolean checkBufferSize(ByteBuffer buffer);
     void accept(FrameVisitor visitor);
     int getOpcode();
+
+    // constant opcodes
+    int CONNECTION_WITH_LOGIN = 0;
+    int CONNECTION_WITH_LOGIN_AND_PASSWORD = 1;
+    int PRIVATE_CONNECTION_REQUEST = 2;
+    int DISCONNECTION_REQUEST = 3;
+    int PRESENTATION_LOGIN = 4;
+    int CONNECTION_WITH_LOGIN_OK = 10;
+    int CONNECTION_WITH_LOGIN_AND_PASSWORD_OK = 11;
+    int CONNECTION_WITH_REGISTER_OK = 12;
+    int PRIVATE_CONNECTION_OK = 13;
+    int PRIVATE_CONNECTION_KO = 14;
+    int DISCONNECTION_OK = 15;
+    int GLOBAL_MESSAGE = 20;
+    int PRIVATE_MESSAGE = 21;
+    int PRIVATE_FILE = 22;
+    int LOGIN_ERROR = 30;
+    int LOGIN_WITH_PASSWORD_ERROR = 31;
+    int INVALID_ADDRESS = 32;
+    int INVALID_PORT = 33;
+    int DISCONNECTION_KO = 34;
 }
