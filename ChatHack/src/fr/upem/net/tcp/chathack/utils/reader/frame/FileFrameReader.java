fc111e2af754aa5fd2422e990fc08cf00f908dd3
Cloudy <cloud.kira@gmail.com>
2020-05-19 13:06:12 +0200

    file frame reader ok

--- ChatHack/src/fr/upem/net/tcp/chathack/utils/reader/frame/FileFrameReader.java
+++ ChatHack/src/fr/upem/net/tcp/chathack/utils/reader/frame/FileFrameReader.java
@@ -1,14 +1,68 @@
 package fr.upem.net.tcp.chathack.utils.reader.frame;
 
 import fr.upem.net.tcp.chathack.utils.frame.FileFrame;
+import fr.upem.net.tcp.chathack.utils.reader.utils.ByteBufferReader;
 import fr.upem.net.tcp.chathack.utils.reader.utils.Reader;
+import fr.upem.net.tcp.chathack.utils.reader.utils.StringReader;
 
 import java.nio.ByteBuffer;
 
 public class FileFrameReader implements Reader<FileFrame> {
+
+    private enum State {
+        DONE, WAITING_FILE_NAME, WAITING_FILE, ERROR
+    }
+
+    private State state = State.WAITING_FILE_NAME;
+    private static final int BUFFER_SIZE = 10_000;
+    private final ByteBuffer internalBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+
+    private String fileName;
+    private ByteBuffer fileData;
+
+    /*
+           int          String       int       byte
+    -------------------------------------------------
+    | SizeOfFileName | FileName | SizeOfFile | File |
+    -------------------------------------------------
+ */
     @Override
-    public ProcessStatus process(ByteBuffer bb) {
-        return null;
+    public ProcessStatus process(ByteBuffer buffer) {
+        if (state == State.DONE || state == State.ERROR) {
+            throw new IllegalStateException();
+        }
+
+        if(state == State.WAITING_FILE_NAME) {
+            StringReader stringReader = new StringReader();
+            ProcessStatus status = stringReader.process(buffer);
+            switch (status) {
+                case DONE:
+                    fileName = stringReader.get();
+                    state = State.WAITING_FILE;
+                    break;
+                case REFILL:
+                    return ProcessStatus.REFILL;
+                case ERROR:
+                    return ProcessStatus.ERROR;
+            }
+        }
+
+        if(state == State.WAITING_FILE) {
+            ByteBufferReader byteBufferReader = new ByteBufferReader();
+            ProcessStatus status = byteBufferReader.process(buffer);
+            switch (status) {
+                case DONE:
+                    fileData = byteBufferReader.get();
+                    state = State.DONE;
+                    break;
+                case REFILL:
+                    return ProcessStatus.REFILL;
+                case ERROR:
+                    return ProcessStatus.ERROR;
+            }
+        }
+
+        return ProcessStatus.DONE;
     }
 
     @Override
@@ -18,7 +72,11 @@ public class FileFrameReader implements Reader<FileFrame> {
 
     @Override
     public FileFrame get(int opcode) {
-        return null;
+        if (state != State.DONE) {
+            throw new IllegalStateException();
+        }
+
+        return FileFrame.createFileFrame(opcode, fileName, fileData);
     }
 
     @Override
